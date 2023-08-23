e5afe7ff30da65f535440bafe0ad192edaabfb4b
Rustem Kurmaev <rustboyar@gmail.com>
2023-08-23 11:35:01 +0200

    feat: move base permissions

---
+++ src/contracts/BaseMovePermissionsPayload.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3Base} from 'aave-address-book/AaveV3Base.sol';
+import {AaveMisc} from 'aave-address-book/AaveMisc.sol';
+
+import {MigratorLib} from './MigratorLib.sol';
+
+contract BaseMovePermissionsPayload {
+  address public constant BASE_LEVEL_1_EXECUTOR_V3 = address(6);
+
+  function execute() external {
+    // V3 MARKETS
+    MigratorLib.migrateV3MarketPermissions(
+      BASE_LEVEL_1_EXECUTOR_V3,
+      AaveV3Base.ACL_MANAGER,
+      AaveV3Base.POOL_ADDRESSES_PROVIDER,
+      AaveV3Base.EMISSION_MANAGER,
+      AaveV3Base.POOL_ADDRESSES_PROVIDER_REGISTRY,
+      AaveV3Base.COLLECTOR,
+      AaveMisc.PROXY_ADMIN_BASE,
+      AaveV3Base.WETH_GATEWAY,
+      address(0), // swap collateral adapter not deployed yet
+      address(0) // repay with collateral adapter not deployed yet
+    );
+  }
+}
---
+++ tests/BaseMovePermissionsPayloadTest.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {MovePermissionsTestBase} from './MovePermissionsTestBase.sol';
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
+import {AaveV3Base, AaveV3BaseAssets} from 'aave-address-book/AaveV3Base.sol';
+import {AaveMisc} from 'aave-address-book/AaveMisc.sol';
+import {ProxyAdmin} from 'solidity-utils/contracts/transparent-proxy/ProxyAdmin.sol';
+import {TransparentUpgradeableProxy} from 'solidity-utils/contracts/transparent-proxy/TransparentUpgradeableProxy.sol';
+import {BaseMovePermissionsPayload} from '../src/contracts/BaseMovePermissionsPayload.sol';
+import {IPoolAddressProviderRegistry} from './helpers/IPoolAddressProviderRegistry.sol';
+import {IEmissionManager} from './helpers/IEmissionManager.sol';
+
+contract BaseMovePermissionsPayloadTest is MovePermissionsTestBase {
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('base'), 2996825);
+  }
+
+  function testPermissionsTransfer() public {
+    BaseMovePermissionsPayload payload = new BaseMovePermissionsPayload();
+
+    GovHelpers.executePayload(vm, address(payload), AaveGovernanceV2.BASE_BRIDGE_EXECUTOR);
+
+    vm.startPrank(payload.BASE_LEVEL_1_EXECUTOR_V3());
+
+    _testV3(
+      payload.BASE_LEVEL_1_EXECUTOR_V3(),
+      AaveV3Base.POOL_ADDRESSES_PROVIDER,
+      AaveV3Base.COLLECTOR,
+      AaveV3BaseAssets.USDbC_UNDERLYING,
+      AaveV3BaseAssets.USDbC_A_TOKEN,
+      AaveV3BaseAssets.USDbC_ORACLE,
+      AaveV3Base.EMISSION_MANAGER,
+      AaveV3Base.POOL_ADDRESSES_PROVIDER_REGISTRY,
+      AaveMisc.PROXY_ADMIN_BASE,
+      AaveV3Base.WETH_GATEWAY,
+      address(0),
+      address(0)
+    );
+
+    vm.stopPrank();
+  }
+}
