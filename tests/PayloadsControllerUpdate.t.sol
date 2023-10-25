7bec04e2e6d1b6aa746f9c428cc1af32d44aa258
sendra <polsendra@gmail.com>
2023-10-25 15:02:27 +0200

    fix: added tests with payloads executions

--- tests/PayloadsControllerUpdate.t.sol
+++ tests/PayloadsControllerUpdate.t.sol
@@ -14,12 +14,23 @@ import {MiscOptimism} from 'aave-address-book/MiscOptimism.sol';
 import {MiscBase} from 'aave-address-book/MiscBase.sol';
 import {MiscGnosis} from 'aave-address-book/MiscGnosis.sol';
 import {MiscMetis} from 'aave-address-book/MiscMetis.sol';
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 import {GovernanceV3Ethereum, GovernanceV3Polygon, GovernanceV3Avalanche, GovernanceV3Arbitrum, GovernanceV3Optimism, GovernanceV3Base, GovernanceV3Metis, GovernanceV3Gnosis, GovernanceV3BNB, AaveGovernanceV2} from 'aave-address-book/AaveAddressBook.sol';
+
+//https://snowtrace.io/address/0x0a5a19f1c4a527773f8b6e7428255dd83b7a687b
+//https://arbiscan.io/address/0xd0f0bc55ac46f63a68f7c27fbfd60792c9571fea
+//https://optimistic.etherscan.io/address/0xab22988d93d5f942fc6b6c6ea285744809d1d9cc
+//https://andromeda-explorer.metis.io/address/0xA9F30e6ED4098e9439B2ac8aEA2d3fc26BcEbb45/
+//https://basescan.org/address/0x80a2f9a653d3990878cff8206588fd66699e7f2a
 
 contract MockImplementation is Initializable {
   uint256 public constant TEST = 1;
 
   function initialize() external reinitializer(2) {}
+}
+
+interface IPayload {
+  function execute() external;
 }
 
 abstract contract BaseTest is Test {
@@ -33,12 +44,12 @@ abstract contract BaseTest is Test {
 
   function shortExecutor() public view virtual returns (address);
 
+  function payload() public view virtual returns (address);
+
   function _setUp() internal {
     pcImpl = new MockImplementation();
 
-    // update owner of proxyAdmin
-    hoax(shortExecutor());
-    Ownable(proxyAdmin()).transferOwnership(executorLvl1());
+    GovHelpers.executePayload(vm, address(payload()), shortExecutor());
 
     hoax(executorLvl1());
     ProxyAdmin(proxyAdmin()).upgradeAndCall(
@@ -70,8 +81,13 @@ contract ProxyAdminTestEthereum is BaseTest {
     return AaveGovernanceV2.SHORT_EXECUTOR;
   }
 
+  function payload() public pure override returns (address) {
+    return 0xE40E84457F4b5075f1EB32352d81ecF1dE77fee6;
+  }
+
   function setUp() public {
     vm.createSelectFork('mainnet', 18427147);
+
     _setUp();
   }
 }
@@ -91,6 +107,10 @@ contract ProxyAdminTestPolygon is BaseTest {
 
   function shortExecutor() public pure override returns (address) {
     return AaveGovernanceV2.POLYGON_BRIDGE_EXECUTOR;
+  }
+
+  function payload() public pure override returns (address) {
+    return 0xc7751400F809cdB0C167F87985083C558a0610F7;
   }
 
   function setUp() public {
